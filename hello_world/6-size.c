de0c6c80687a77c0947d36c7924795818b233690
ElWillyGod <manuelantognazza@gmail.com>
2024-02-14 15:55:34 -0300

    ss

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int masLargoInt;
 	float punto_Flotante;
 
-	ptintf("Size of a char: %lu byte(s)\n", (using long)sizeof(caracter));
-	ptintf("Size of a int: %lu byte(s)\n", (using long)sizeof(numero));
-	ptintf("Size of a long int: %lu byte(s)\n", (using long)sizeof(largoint));
-	ptintf("Size of a long long int: %lu byte(s)\n", (using long)sizeof(masLargoInt));
-	ptintf("Size of a float: %lu byte(s)\n", (using long)sizeof(punto_Flotante));
+	ptintf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(caracter));
+	ptintf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(numero));
+	ptintf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(largoint));
+	ptintf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(masLargoInt));
+	ptintf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(punto_Flotante));
 
 	return (0);
 }
