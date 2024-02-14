6eba4850621e365b7693d561b2d740bf8ec474ef
ElWillyGod <manuelantognazza@gmail.com>
2024-02-14 16:00:01 -0300

    ss

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	ptintf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	ptintf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	ptintf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	ptintf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-	ptintf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
