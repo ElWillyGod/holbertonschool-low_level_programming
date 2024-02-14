c97ab63a71f65e199a7958d471c1f2be36b11b97
ElWillyGod <manuelantognazza@gmail.com>
2024-02-14 15:59:20 -0300

    ss

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,19 +4,20 @@
 *main - cositas
 *Return: mas coasa
 */
+
 int main(void)
 {
-	int numero;
-	char caracter;
-	long int largoint;
-	long long int masLargoInt;
-	float punto_Flotante;
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
 
-	ptintf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(caracter));
-	ptintf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(numero));
-	ptintf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(largoint));
-	ptintf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(masLargoInt));
-	ptintf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(punto_Flotante));
+	ptintf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	ptintf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	ptintf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	ptintf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	ptintf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
