c0c1f2d6b4cdb40d00cb9674ae19df9866287da5
ElWillyGod <manuelantognazza@gmail.com>
2024-02-14 15:53:43 -0300

    ss

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+*main - cositas
+*Return: mas coasa
+*/
+int main(void)
+{
+	int numero;
+	char caracter;
+	long int largoint;
+	long long int masLargoInt;
+	float punto_Flotante;
+
+	ptintf("Size of a char: %lu byte(s)\n", (using long)sizeof(caracter));
+	ptintf("Size of a int: %lu byte(s)\n", (using long)sizeof(numero));
+	ptintf("Size of a long int: %lu byte(s)\n", (using long)sizeof(largoint));
+	ptintf("Size of a long long int: %lu byte(s)\n", (using long)sizeof(masLargoInt));
+	ptintf("Size of a float: %lu byte(s)\n", (using long)sizeof(punto_Flotante));
+
+	return (0);
+}
