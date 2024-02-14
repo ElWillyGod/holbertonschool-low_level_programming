e5089b2f1d52b972ff193e74467514ff3aca2c58
ElWillyGod <manuelantognazza@gmail.com>
2024-02-14 12:56:45 -0300

    ss

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
+#include<string.h>
+
 int main(void){
 
-puts ("\"Programming is like building a multilingual puzzle");
+char mensaje ="\"Programming is like building a multilingual puzzle";
+puts (mensaje);
 
 return 0;
 }
