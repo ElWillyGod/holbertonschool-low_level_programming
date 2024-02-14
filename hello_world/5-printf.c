87e4e98fd597c32c6bd4205b7d959a27083aed79
ElWillyGod <manuelantognazza@gmail.com>
2024-02-14 15:21:39 -0300

    ss

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<string.h>
+/**
+*main - mostrar cosas
+*Return: retornamos el codogo de error
+*/
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
