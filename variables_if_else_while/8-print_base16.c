07c2c184cabf7faedb30a2b752e34ab565ecae78
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 13:06:32 -0300

    sa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+*main - cosas
+*Return: sadsad
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 102; i++)
+	{
+	if ((i < 58) && (i < 96))
+		putchar(i)
+	}
+
+	puchear(10);
+
+	return (0);
+}
