f4d3396f085613f023131449a676b7bb5d8079f8
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 11:50:26 -0300

    sa

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - coasa
+*Return: mas coasa
+*/
+int main(void)
+{
+	char c = 48;
+
+	while (c <= 57)
+	{
+	printf("%c", c);
+	c++;
+	}
+
+	printf("\n");
+	return (0);
+}
