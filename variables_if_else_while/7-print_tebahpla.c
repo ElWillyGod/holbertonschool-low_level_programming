55b909f341429f39c5d91aeaeef5bbd3f376e49d
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 12:41:04 -0300

    sa

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main-printnumerossinchar
+*Return:sogigo
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 57 ; i >= 48 ; i--)
+	{
+	putchar(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
