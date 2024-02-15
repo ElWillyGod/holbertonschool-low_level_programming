82d59cd702b77114fecfea9272f0107a3efec80b
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 13:49:21 -0300

    sa

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 56; i++)
+	for (i = 48; i <= 57; i++)
 	{
 	putchar(i);
 	putchar(44);
 	putchar(32);
 	}
 
-	putchar(57);
+	putchar(10);
 
 	return (0);
 }
