d8e5f214edbd52fc6db713b48fc056304059e7eb
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 09:37:40 -0300

    cosas

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+*main - algo
+*Return: que signo tiene el numero
+*/
+
+int main(int n)
+{
+	if (n < 0)
+	{
+	printf("%d is negative", n);
+	}
+	 else
+	{
+	if (n = 0)
+	{
+	print("%d is zero", n);
+	}
+	else
+	{
+	printf("%d is positive", n);
+	}
+
+	return (0);
+}
