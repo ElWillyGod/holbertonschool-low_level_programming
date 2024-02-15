6818ae8b3869ced05a718e5b4d2bb003ae601a32
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 12:00:10 -0300

    sa

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - print numeros sin char
+*Return: sogigo
+*/
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+	printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
