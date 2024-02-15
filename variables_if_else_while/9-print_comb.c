e7edfb071e1bb491db750402223b7f962799db94
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 13:41:20 -0300

    sa

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+*main - asd
+*Return: asdsad
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 56; i++)
+	{
+	putchar(i);
+	putchar(32);
+	putchar(44);
+	}
+
+	putchar(57);
+
+	return (0);
+}
