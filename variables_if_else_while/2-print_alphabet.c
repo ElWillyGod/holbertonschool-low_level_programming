9cbbd63b3b6ecc07560df1e0a7fdfb0e4e0ce191
ElWillyGod <manuelantognazza@gmail.com>
2024-02-15 11:17:16 -0300

    sa

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - mostrar las letras
+*Return: coigo de error
+*/
+
+int main(void)
+{
+	char letra = '97'
+
+	while (letra <= 122)
+	{
+	printf("%c", letra);
+	letra++;
+	}
+
+	return (0);
+}
