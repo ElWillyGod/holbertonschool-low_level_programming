d86ca06052ed796230b0f97e58d8efc4b3814918
ElWillyGod <manuelantognazza@gmail.com>
2024-02-16 14:03:17 -0300

    ss

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,16 +10,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		printf("+");
 		return (1);
 	}
 	else
 	{
 		if (n < 0)
 		{
+			printf("-");
 			return (-1);
 		}
 		else
 		{
+			printf("0");
 			return (0);
 		}
 	}
