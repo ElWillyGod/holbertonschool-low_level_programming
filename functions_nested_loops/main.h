46126e222482d66ea6b3b59dfe332bd3c5e1e71c
ElWillyGod <manuelantognazza@gmail.com>
2024-02-16 09:58:41 -0300

    ss

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
-*_putchar - cosas
+*print_alphabet - cosas
 *Return: ni idea
 */
+void print_alphabet(void)
+}
+	char letra = 97;
 
+	while (letra <= 122)
+	{
+	printf("%c", letra);
+	letra++;
+	}
+	printf("\n");
+	return (0);
+}
 #endif /* MAIN_H */
