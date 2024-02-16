f48f2388357fbea6c7f102be7c553bd63c5420b1
ElWillyGod <manuelantognazza@gmail.com>
2024-02-16 10:38:51 -0300

    ss

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - imprimir 10 veces el alphabet
+*Return: ni idea
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	print_alphabet();
+	}
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,11 +2,19 @@
 
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
 *print_alphabet - cosas
 *Return: ni idea
 */
 
 void print_alphabet(void);
+
+/**
+*print_alphabet_x10 - imprimimos el alphabet 10 veces
+*Return: nada
+*/
+
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
