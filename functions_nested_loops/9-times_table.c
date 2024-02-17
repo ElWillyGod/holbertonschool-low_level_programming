027721eb5bb9b1c3842a98952de163ca0483a96a
ElWillyGod <manuelantognazza@gmail.com>
2024-02-17 09:01:55 -0500

    ss

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,7 +15,14 @@ void times_table(void)
 		{
 			if (j != 0)
 			{
-				printf("%2d, ", i * j);
+				if (j != 9)
+				{
+					printf("%2d, ", i * j);
+				}
+				else
+				{
+					printf("%2d", i * j);
+				}
 			}
 			else
 			{
