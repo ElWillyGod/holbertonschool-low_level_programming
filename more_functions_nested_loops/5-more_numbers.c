5184d56557c97057db1edebeca7aca0739a4b72e
ElWillyGod <manuelantognazza@gmail.com>
2024-02-17 13:03:21 -0500

    ss

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,14 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			_putchar(j + '0');
+		}
+
+		for (j = 10; j <= 14; j++)
+		{
+			printf("%d", j);
 		}
 
 		_putchar(10);
