7643685da89116e845346fcfda6cfbf3ab009604
ElWillyGod <manuelantognazza@gmail.com>
2024-02-17 11:50:07 -0500

    ss

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *_isupper - es o no es mayuscula
+ *@c: numero
+ *Return: es o no es mayuscula
+ */
+
+int _isupper(int c)
+{
+	return (isupper(c) == 1 ? 1 : 0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ *_isupper - vermo si es mayuscula o no?
+ *@c: letra en int
+ *Return: retornamos si es mayuscula o no
+ */
+
+int _isipper(int c);
