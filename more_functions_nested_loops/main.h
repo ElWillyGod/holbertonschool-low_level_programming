e87b93f2bde4cd6b94ae08dde6fdd184b350659b
ElWillyGod <manuelantognazza@gmail.com>
2024-02-17 12:26:54 -0500

    ss

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
+
 /*
  *_isupper - vermo si es mayuscula o no?
  *@c: letra en int
