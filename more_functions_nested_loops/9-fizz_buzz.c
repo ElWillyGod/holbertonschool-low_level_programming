4d7b9026a7f8fc598da0198d03d7607d61973c58
ElWillyGod <manuelantognazza@gmail.com>
2024-02-17 17:56:43 -0500

    ss

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (((n % 2) == 0) && ((n % 5) == 0))
+		if (((n % 3) == 0) && ((n % 5) == 0))
 		{
 			printf("FizzBuzz ");
 		}
 		else
 		{
-			if ((n % 2) == 0)
+			if ((n % 3) == 0)
 			{
 				printf("Fizz ");
 			}
