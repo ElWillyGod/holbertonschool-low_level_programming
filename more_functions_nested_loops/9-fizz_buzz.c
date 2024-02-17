7a7dd3a62e39d5ec863d3dee34ea6ffa43dff691
ElWillyGod <manuelantognazza@gmail.com>
2024-02-17 17:53:14 -0500

    ss

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - mostar fizz si es multiplo de 3
+ * 	- Buzz si es multiplo de 5
+ * Return: void
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (((n % 2) == 0) && ((n % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			if ((n % 2) == 0)
+			{
+				printf("Fizz ");
+			}
+			else
+			{
+				if((n % 5) == 0)
+				{
+					printf("Buzz ");
+				}
+				else
+				{
+					printf("%d ", n):
+				}
+			}
+		}
+	}
+}
