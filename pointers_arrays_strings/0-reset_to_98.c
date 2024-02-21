ac8e97b1959e599fd9aeeecb5fb62c25ad916676
root <manuelantognazza@gmail.com>
2024-02-21 12:09:23 -0500

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - camba a 98
+ * @n: puntero a int
+ * Return: nada
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - cambiar numero a 98
+ * @n: puntero
+ * Return: nada
+ */
+
+void reset_to_98(int *n);
