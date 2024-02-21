3a2695086ed2e1f824e765c0c34bca88c1953fa4
root <manuelantognazza@gmail.com>
2024-02-21 13:03:48 -0500

    0

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - largo del string
+ * @s: largo de char
+ * Return: largo de chars
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * reset_to_98 - cambiar numero a 98
@@ -12,7 +13,15 @@ void reset_to_98(int *n);
  * swap_int - intercambiamoms ints
  * @a: numero uno
  * @b: numero dos
- * Return: nada, solo imprime
+ * Return: nada, solo imprim#includee
  */
 
 void swap_int(int *a, int *b);
+
+/**
+ * _strlen - largo de la cadena
+ * @s - cadena
+ * Return: el largo de la cadena 
+ */
+
+int _strlen(char *s);
