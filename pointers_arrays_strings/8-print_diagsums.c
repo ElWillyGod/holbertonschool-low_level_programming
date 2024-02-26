5ac2e17f7a68f5beaecb8bee76cf82ba09f95d35
root <manuelantognazza@gmail.com>
2024-02-26 18:42:02 -0300

    8-print_diagsums.1

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,12 +12,17 @@
 void print_diagsums(int *a, int size)
 {
 	int traza, trazant;
-	int i;
+	int i, indice;
 
 	for (i = 0; i < size; i++)
 	{
-		traza = traza + a[i][i];
-		trazant = trazant +  a[i][size - i - 1];
+		indice = i * size + i;
+
+		traza += a[indice];
+
+		indice = (i + 1) * size - (i + 1);
+
+		trazant += a[indice];
 	}
 
 	printf("%d, %d\n", traza, trazant);
