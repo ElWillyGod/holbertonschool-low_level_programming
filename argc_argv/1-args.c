6831a486aa22d51628a93cc38163b7cfa1850df7
root <manuelantognazza@gmail.com>
2024-02-28 10:00:19 -0300

    1-args.1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - mostrar el nomber del programa
+ * @argc: largo
+ * @argv: array con los datos
+ *
+ * Return: mensaje de error
+ */
+
+int main(int argc, char *argv[])
+{
+        (void) argv;
+
+        printf("%d\n", argc - 1);
+
+        return (0);
+}
