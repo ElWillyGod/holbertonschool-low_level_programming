b473a80226fc812ad42b6569f3a588661c259048
root <manuelantognazza@gmail.com>
2024-02-27 22:16:45 -0300

    6.is_prime_number.2

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -17,19 +17,12 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
+	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0) || (n % 7 == 0))
 	{
 		return (0);
 	}
 	else
 	{
-		if (n % 7 == 0)
-		{
-			return (0);
-		}
-		else
-		{
-			return (1);
-		}
+		return (1);
 	}
 }
