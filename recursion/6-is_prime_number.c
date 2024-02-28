323c97bf278a15c4fcae767ef47591ce3cf167cf
root <manuelantognazza@gmail.com>
2024-02-27 22:15:00 -0300

    6.is_prime_number.1

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - imprime 1 si el numero es primo y 0 si no
+ * @n: numero a evaluar
+ *
+ * Return: retorna 1 si es primo y 0 si no
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
+	{
+		return (0);
+	}
+	else
+	{
+		if (n % 7 == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+}
--- recursion/main.h
+++ recursion/main.h
@@ -69,4 +69,13 @@ int _pow_recursion(int x, int y);
 
 int _sqrt_recursion(int n);
 
+/**
+ * is_prime_number - imprime 1 si el numero es primo y 0 si no
+ * @n: numero a evaluar
+ *
+ * Return: retorna 1 si es primo y 0 si no
+ */
+
+int is_prime_number(int n);
+
 #endif /* MAIN_H*/
