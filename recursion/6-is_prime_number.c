38c460e0efc6a7e36e9732a2895a76170a125216
root <manuelantognazza@gmail.com>
2024-02-27 22:34:34 -0300

    6.is_prime_number.3

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,33 @@
 #include "main.h"
 
 /**
+ * primo - es primo?
+ * @i: posible primo
+ * @j: posible evaluacion de primo
+ *
+ * Return: int, 1 si es primo, 0 si no
+ */
+
+int primo(int i, int j)
+{
+	if (i <= 1)
+	{
+		return (0);
+	}
+	if ((i % j == 0) && (j > 1))
+	{
+		return (0);
+	}
+	if ((i / j) < j)
+	{
+		return (1);
+	}
+
+	return (primo(i, j + 1));
+}
+
+
+/**
  * is_prime_number - imprime 1 si el numero es primo y 0 si no
  * @n: numero a evaluar
  *
@@ -9,20 +36,5 @@
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	if (n == 2)
-	{
-		return (1);
-	}
-	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0) || (n % 7 == 0))
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+	return (primo(n ,1));
 }
