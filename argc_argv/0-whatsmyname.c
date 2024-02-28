08b2abba7cd7e709bed3494e64186fbd7919557a
root <manuelantognazza@gmail.com>
2024-02-28 09:54:26 -0300

    0-whatsmyname.2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - mostrar el nomber del programa
+ * @argc: largo
+ * @argv: array con los datos
+ *
+ * Return: mensaje de error
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) argc;
