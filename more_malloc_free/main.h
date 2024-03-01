34111012065075983bdaa94bc57d40160199392a
root <manuelantognazza@gmail.com>
2024-03-01 14:49:05 -0300

    3-array_range.1

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -29,4 +29,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+/**
+ * array_range - crear un array de int'
+ * @min: minimo del rango
+ * @max: maximo del rango
+ *
+ * Return: retornamos el puntero al arreglo;
+ */
+
+int *array_range(int min, int max);
+
 #endif /* MAIN_H */
