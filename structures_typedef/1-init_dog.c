4ae354ca6226e950e13c71ae51c45715184e6e4f
root <manuelantognazza@gmail.com>
2024-03-02 17:05:23 -0300

    1-init_dog.1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - asignar valores a un dog
+ * @d: puntero a dog;
+ * @name: parametro nombre;
+ * @age: parametro edad;
+ * @owner: parametro prpr;
+ *
+ * Return: void, solo carga los valores
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,5 +20,17 @@ struct dog
 
 typedef struct dog *Tdog;
 
+/**
+ * init_dog - asignar valores a un dog
+ * @d: puntero a dog;
+ * @name: parametro nombre;
+ * @age: parametro edad;
+ * @owner: parametro prpr;
+ *
+ * Return: void, solo carga los valores
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
 
