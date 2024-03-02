f29164c96ac6804f65dfa71233826192b1a2cbf3
root <manuelantognazza@gmail.com>
2024-03-02 16:51:18 -0300

    0.dog.1

---
+++ structures_typedef/RADME.md
@@ -0,0 +1,1 @@
+Estructuras de datos con typdef y struct
---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * rep_dog - estructura de un perro
+ * @name: nombre
+ * @age: edad
+ * @owner: prop
+ *
+ * Description: info de un perro
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog* Tdog;
+
+#endif /* DOG_H */
+
