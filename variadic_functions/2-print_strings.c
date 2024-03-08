2690cb19cf10d39087b219cb970ab2372aa1829a
root <manuelantognazza@gmail.com>
2024-03-08 11:47:01 -0300

    print_strings.1

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+*print_strings - imprime los parametros string;
+*@separator: separador entre parametros;
+*@n: cantidad de parametros;
+*
+*Return: void, solo muestra los parametros char
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list para;
+	unsigned int i;
+	char *esChar;
+
+	va_start(para, n);
+
+	for (i = 0; i < n; i++)
+	{
+		esChar = va_arg(para, char*);
+
+		if (esChar != NULL)
+		{
+			printf("%s",esChar);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+
+	va_end(para);
+}
