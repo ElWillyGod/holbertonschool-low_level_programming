365a94547b6329f77346ffe4524ab6645ef8a7ba
root <manuelantognazza@gmail.com>
2024-03-30 20:58:05 -0300

    si se pudo burro.2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -34,42 +34,40 @@ void CloseFile(int DescFile)
 
 int main(int argc, char *argv[])
 {
-	int FileFrom, FileTo, ByteRead, ByteWrite;
-	char *buffer;
+	int FileFrom, FileTo, ByteRead;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDIN_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+
 	FileFrom = open(argv[1], O_RDONLY);
-	ByteRead = read(FileFrom, buffer, 1024);
+
+	if ((!argv[1]) || (FileFrom == -1))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
 	FileTo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	do {
-		if ((FileFrom == -1) || (ByteRead == -1))
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-		ByteWrite = write(FileTo, buffer, ByteRead);
-		if ((FileTo == -1) || (ByteWrite == -1))
+
+	while ((ByteRead = read(FileFrom, buffer, 1024)) > 0)
+	{
+		if ((write(FileTo, buffer, ByteRead) != ByteRead) || FileTo == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
 			exit(99);
 		}
-		ByteRead = read(FileFrom, buffer, 1024);
-		FileTo = open(argv[2], O_WRONLY | O_APPEND);
-	} while (ByteRead > 0);
+	}
 
-	free(buffer);
+	if (ByteRead == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
+	}
+
 	CloseFile(FileFrom);
 	CloseFile(FileTo);
 
